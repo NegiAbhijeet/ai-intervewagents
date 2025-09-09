76169bccdf1d231df56ad70aa830f38d9b201696
Abhijeet Negi <abhijeetnegi.ab@gmail.com>
2025-09-09 18:03:44 +0530

    nothing

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -21,7 +21,7 @@ import fetchUserDetails from '../libs/fetchUser';
 import fetchWithAuth from '../libs/fetchWithAuth';
 import { API_URL } from '../components/config';
 import { AppStateContext } from '../components/AppContext';
-
+import LinearGradient from 'react-native-linear-gradient';
 import {
   GoogleSignin,
   statusCodes,
@@ -93,7 +93,7 @@ const LoginScreen = () => {
       }
 
       const final = await fetchUserDetails(user.uid);
-      console.log("====3",final)
+      console.log('====3', final);
       setUserProfile(final);
     } catch (error) {
       console.error('[Google Sign-In] Error:', error);
@@ -145,8 +145,10 @@ const LoginScreen = () => {
 
   return (
     <View className="flex-1 justify-center px-6 bg-white">
-      <Text className="text-2xl font-bold mb-2">Login</Text>
-      <Text className="text-gray-500 mb-6">
+      <Text className="text-3xl font-bold text-gray-800 mb-2 text-center">
+        Welcome Back!
+      </Text>
+      <Text className="text-gray-500 text-center mb-6">
         Enter your email and password to access your account
       </Text>
 
@@ -154,7 +156,8 @@ const LoginScreen = () => {
         <Text className="text-sm font-medium text-gray-700 mb-1">Email</Text>
         <TextInput
           placeholder="name@example.com"
-          className="bg-gray-100 px-4 py-2 rounded-md"
+          placeholderTextColor="#aaa"
+          className="bg-blue-50 px-4 py-2 rounded-xl text-gray-800 border border-blue-200"
           value={email}
           onChangeText={setEmail}
           keyboardType="email-address"
@@ -165,33 +168,43 @@ const LoginScreen = () => {
       <View className="mb-4">
         <View className="flex-row justify-between mb-1">
           <Text className="text-sm font-medium text-gray-700">Password</Text>
-          <TouchableOpacity>
-            <Text className="text-xs text-indigo-600">Forgot password?</Text>
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Text className="text-xs text-blue-500">
+              {showPassword ? 'Hide' : 'Show'}
+            </Text>
           </TouchableOpacity>
         </View>
-        <View className="flex-row items-center bg-gray-100 rounded-md px-3">
+        <View className="flex-row items-center bg-blue-50 rounded-xl px-3 border border-blue-200">
           <TextInput
             placeholder="••••••••"
-            className="flex-1 py-2"
+            placeholderTextColor="#aaa"
+            className="flex-1 py-2 text-gray-800"
             secureTextEntry={!showPassword}
             value={password}
             onChangeText={setPassword}
           />
         </View>
       </View>
-
-      <TouchableOpacity
-        className="bg-gradient-to-r from-blue-400/60 to-pink-500/70 py-3 rounded-md flex-row justify-center items-center mb-4"
-        disabled={isEmailLoading || isGoogleLoading}
-        onPress={handleSubmit}
-      >
-        {isEmailLoading ? (
-          <ActivityIndicator color="#fff" />
-        ) : (
-          <Text className="text-white font-semibold">Login</Text>
-        )}
-      </TouchableOpacity>
-
+      <View className="rounded-md overflow-hidden h-16">
+        <LinearGradient
+          colors={['#60a5fa', '#ec4899']} // blue to pink
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          className="rounded-md mb-4 h-full pt-4"
+        >
+          <TouchableOpacity
+            className="flex-row justify-center items-center"
+            disabled={isEmailLoading || isGoogleLoading}
+            onPress={handleSubmit}
+          >
+            {isEmailLoading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Text className="text-white font-semibold text-xl">Login</Text>
+            )}
+          </TouchableOpacity>
+        </LinearGradient>
+      </View>
       <View className="flex-row items-center my-4">
         <View className="flex-1 h-px bg-gray-300" />
         <Text className="mx-2 text-xs text-gray-400">OR</Text>
@@ -199,14 +212,16 @@ const LoginScreen = () => {
       </View>
 
       <TouchableOpacity
-        className="bg-white border border-gray-300 py-3 rounded-md flex-row justify-center items-center"
+        className="bg-white py-3 rounded-xl flex-row justify-center items-center border border-gray-300 shadow-sm"
         onPress={loginWithGoogle}
         disabled={isEmailLoading || isGoogleLoading}
       >
         {isGoogleLoading ? (
           <ActivityIndicator />
         ) : (
-          <Text className="text-black font-semibold">Continue with Google</Text>
+          <Text className="text-black font-semibold text-lg">
+            Continue with Google
+          </Text>
         )}
       </TouchableOpacity>
 
