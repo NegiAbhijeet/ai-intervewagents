2b11d038d2a5a6e022f883f17ef43988331eb0e1
Abhijeet Negi <abhijeetnegi.ab@gmail.com>
2025-09-11 00:35:06 +0530

    add icons in interview

--- components/ScheduleInterviewScreen.jsx
+++ components/ScheduleInterviewScreen.jsx
@@ -14,6 +14,7 @@ import { Picker } from '@react-native-picker/picker';
 import debounce from 'lodash.debounce';
 import { JAVA_API_URL, API_URL } from './config';
 import InterviewScreen from './interviewScreen';
+import Ionicons from '@react-native-vector-icons/ionicons';
 const typeOptions = [
   { label: 'Technical', value: 'Technical' },
   { label: 'Behavioral', value: 'Behavioral' },
@@ -236,124 +237,123 @@ export default function ScheduleInterviewScreen({ userProfile }) {
   };
 
   return (
-    <ScrollView className="flex-1" showsVerticalScrollIndicator={false}>
-      <Text className="text-2xl font-bold mb-4">Start Interview</Text>
+ <ScrollView className="flex-1" showsVerticalScrollIndicator={false}>
+  <Text className="text-2xl font-bold mb-4">
+     Start Interview
+  </Text>
 
-      {showInterviewScreen && interviewData?.meetingId && (
-        <InterviewScreen
-          {...interviewData}
-          candidateName={candidateName}
-          adminId={userProfile?.uid}
-          showInterviewScreen={showInterviewScreen}
-          setShowInterviewScreen={setShowInterviewScreen}
-        />
-      )}
+  {showInterviewScreen && interviewData?.meetingId && (
+    <InterviewScreen
+      {...interviewData}
+      candidateName={candidateName}
+      adminId={userProfile?.uid}
+      showInterviewScreen={showInterviewScreen}
+      setShowInterviewScreen={setShowInterviewScreen}
+    />
+  )}
 
-      <Text className="text-lg font-semibold text-slate-700 mb-2">
-        <Icon name="zap" size={18} color="#10b981" /> Interview Type *
-      </Text>
-      <View className="border border-slate-300 rounded-xl bg-white mb-3 px-1">
-        <Picker
-          selectedValue={interviewType}
-          onValueChange={itemValue => setInterviewType(itemValue)}
-          style={{ color: '#334155' }} 
-          itemStyle={{ color: '#334155'}} 
-          dropdownIconColor="#334155" 
+  <Text className="text-lg font-semibold text-slate-700 mb-2">
+    <Ionicons name="layers-outline" size={18} color="#10b981" /> Interview Type *
+  </Text>
+  <View className="border border-slate-300 rounded-xl bg-white mb-3 px-1">
+    <Picker
+      selectedValue={interviewType}
+      onValueChange={itemValue => setInterviewType(itemValue)}
+      style={{ color: '#334155' }}
+      itemStyle={{ color: '#334155' }}
+      dropdownIconColor="#334155"
+    >
+      <Picker.Item label="Select Type" style={{ fontSize: 16 }} value="" />
+      {typeOptions.map((item, index) => (
+        <Picker.Item key={index} label={item?.label} value={item?.value} />
+      ))}
+    </Picker>
+  </View>
+
+  <Text className="text-lg font-semibold text-slate-700 mt-4 mb-2">
+    <Ionicons name="briefcase-outline" size={18} color="#10b981" /> Position *
+  </Text>
+  <TextInput
+    value={selectedPosition}
+    onChangeText={handlePositionChange}
+    placeholder="Type a position..."
+    className="h-16 bg-white rounded-xl border border-slate-300 px-3 text-lg text-black"
+  />
+
+  <Text className="text-lg font-semibold text-slate-700 mt-4 mb-2">
+    <Ionicons name="code-slash-outline" size={18} color="#10b981" /> Add Skills *
+  </Text>
+  <View className="flex-row items-center">
+    <TextInput
+      value={currentSkill}
+      onChangeText={setCurrentSkill}
+      placeholder="e.g. JavaScript, React"
+      className="flex-1 h-16 bg-white rounded-xl border border-slate-300 px-3 text-lg text-black"
+      onSubmitEditing={addSkill}
+    />
+    <TouchableOpacity
+      onPress={addSkill}
+      className="ml-3 bg-gray-100 border border-slate-300 p-3 rounded-xl h-16 w-16 flex items-center justify-center"
+    >
+      <Ionicons name="add" size={24} color="#3B82F6" />
+    </TouchableOpacity>
+  </View>
+
+  {loadingSkills ? (
+    <ActivityIndicator color="#3b82f6" className="my-3" />
+  ) : (
+    <View className="flex-row flex-wrap mt-3">
+      {skills.map(skill => (
+        <View
+          key={skill}
+          className="flex-row items-center bg-blue-100 px-3 py-2 rounded-full m-1"
         >
-          <Picker.Item label="Select Type" style={{ fontSize: 16 }} value="" />
-          {typeOptions.map((item, index) => (
-            <Picker.Item key={index} label={item?.label} value={item?.value} />
-          ))}
-        </Picker>
-      </View>
-
-      <Text className="text-lg font-semibold text-slate-700 mt-4 mb-2">
-        <Icon name="user-check" size={16} color="#10b981" /> Position *
-      </Text>
-      <TextInput
-        value={selectedPosition}
-        onChangeText={handlePositionChange}
-        placeholder="Type a position..."
-        className="h-16 bg-white rounded-xl border border-slate-300 px-3 text-lg text-black"
-      />
-
-      <Text className="text-lg font-semibold text-slate-700 mt-4 mb-2">
-        <Icon name="code" size={16} color="#10b981" /> Add Skills *
-      </Text>
-      <View className="flex-row items-center">
-        <TextInput
-          value={currentSkill}
-          onChangeText={setCurrentSkill}
-          placeholder="e.g. JavaScript, React"
-          className="flex-1 h-16 bg-white rounded-xl border border-slate-300 px-3 text-lg text-black"
-          onSubmitEditing={addSkill}
-        />
-        <TouchableOpacity
-          onPress={addSkill}
-          className="ml-3 bg-gray-100 border border-slate-300 p-3 rounded-xl h-16 w-16 flex items-center justify-center"
-        >
-          <Text className="text-2xl">+</Text>
-        </TouchableOpacity>
-      </View>
-
-      {loadingSkills ? (
-        <ActivityIndicator color="#3b82f6" className="my-3" />
-      ) : (
-        <View className="flex-row flex-wrap mt-3">
-          {skills.map(skill => (
-            <View
-              key={skill}
-              className="flex-row items-center bg-blue-100 px-3 py-2 rounded-full m-1"
-            >
-              <Text className="text-xs text-blue-900 mr-2">{skill}</Text>
-              <TouchableOpacity onPress={() => removeSkill(skill)}>
-                <Text className="text-white text-xs bg-red-500 rounded-full px-2">
-                  X
-                </Text>
-              </TouchableOpacity>
-            </View>
-          ))}
+          <Text className="text-xs text-blue-900 mr-2">{skill}</Text>
+          <TouchableOpacity onPress={() => removeSkill(skill)}>
+            <Ionicons name="close-circle" size={16} color="#EF4444" />
+          </TouchableOpacity>
         </View>
-      )}
+      ))}
+    </View>
+  )}
 
-      <Text className="text-lg font-semibold text-slate-700 mt-4 mb-2">
-        <Icon name="clock" size={16} color="#10b981" /> Duration (in minutes) *
-      </Text>
-      <TextInput
-        value={duration.toString()}
-        onChangeText={val => {
-          // const num = parseInt(val, 10);
+  <Text className="text-lg font-semibold text-slate-700 mt-4 mb-2">
+    <Ionicons name="time-outline" size={18} color="#10b981" /> Duration (in minutes) *
+  </Text>
+  <TextInput
+    value={duration.toString()}
+    onChangeText={val => setDuration(val)}
+    // const num = parseInt(val, 10);
           // if (!isNaN(num) && num >= 0 && num <= 60) setDuration(num); check while validation
-          setDuration(val);
-        }}
-        keyboardType="numeric"
-        placeholder="15 or 30"
-        className="h-16 bg-white rounded-xl border border-slate-300 px-3 text-lg text-black"
-      />
+    keyboardType="numeric"
+    placeholder="15 or 30"
+    className="h-16 bg-white rounded-xl border border-slate-300 px-3 text-lg text-black"
+  />
 
-      <TouchableOpacity
-        onPress={handleSubmit}
-        disabled={isLoading}
-        className={`mt-8 h-16 rounded-xl gap-2 flex-row items-center justify-center ${
-          isLoading ? 'bg-slate-400' : 'bg-blue-500'
-        }`}
-      >
-        {isLoading ? (
-          <>
-            <Text className="text-white font-semibold text-lg ml-2 py-4">
-              Please wait...
-            </Text>
-            <ActivityIndicator color="#fff" />
-          </>
-        ) : (
-          <>
-            <Icon name="play" size={18} color="#fff" />
-            <Text className="text-white font-semibold text-lg ml-2 py-4">
-              Start Interview
-            </Text>
-          </>
-        )}
-      </TouchableOpacity>
-    </ScrollView>
+  <TouchableOpacity
+    onPress={handleSubmit}
+    disabled={isLoading}
+    className={`mt-8 h-16 rounded-xl gap-2 flex-row items-center justify-center ${
+      isLoading ? 'bg-slate-400' : 'bg-blue-500'
+    }`}
+  >
+    {isLoading ? (
+      <>
+        <Text className="text-white font-semibold text-lg ml-2 py-4">
+          Please wait...
+        </Text>
+        <ActivityIndicator color="#fff" />
+      </>
+    ) : (
+      <>
+        <Ionicons name="play-circle-outline" size={22} color="#fff" />
+        <Text className="text-white font-semibold text-lg ml-2 py-4">
+          Start Interview
+        </Text>
+      </>
+    )}
+  </TouchableOpacity>
+</ScrollView>
+
   );
 }
