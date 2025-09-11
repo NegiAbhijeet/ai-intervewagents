a7f9aa116c89d5b1d7c484fa8b6a500900be6f21
Abhijeet Negi <abhijeetnegi.ab@gmail.com>
2025-09-11 14:29:49 +0530

    route params

--- pages/Interview.tsx
+++ pages/Interview.tsx
@@ -1,11 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Layout from './Layout';
 import ScheduleInterviewScreen from '../components/ScheduleInterviewScreen';
 import { AppStateContext } from '../components/AppContext';
+
 const Interview = ({ route }) => {
   const { userProfile } = useContext(AppStateContext);
-  const { type } = route.params;
-  console.log(type);
+  const [type, setType] = useState(route.params?.type || 'Practice');
+
+  useEffect(() => {
+    if (route.params?.type) {
+      setType(route.params.type);
+    }
+  }, [route.params?.type]);
+
   return (
     <Layout>
       <ScheduleInterviewScreen userProfile={userProfile} type={type} />
